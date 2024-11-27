4efdad2f5108109abbb7a858ac5be24bab408da3
marbel <othmane.xx90@gmail.com>
2024-11-27 21:53:26 +0100

    accumulate

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Homework 1:
 """
-from operator import add, sub
+from operator import add, sub, mul
 
 def a_plus_abs_b(a,b):
     if b<0:
@@ -56,12 +56,63 @@ def product(n, term):
         p *= term(i)
     return p
 
+def accumulate(fuse, start, n, term):
+    """Return the result of fusing together the first n terms in a sequence 
+    and start.  The terms to be fused are term(1), term(2), ..., term(n). 
+    The function fuse is a two-argument commutative & associative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11 (fuse is never used)
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    """
+    r = start
+    for i in range(1,n+1):
+        r = fuse(r,term(i))
+    return r
+
+def summation_using_accumulate(n, term):
+    """Returns the sum: term(1) + ... + term(n), using accumulate.
+
+    >>> summation_using_accumulate(5, square) # square(1) + square(2) + ... + square(4) + square(5)
+    55
+    >>> summation_using_accumulate(5, triple) # triple(1) + triple(2) + ... + triple(4) + triple(5)
+    45
+    >>> # This test checks that the body of the function is just a return statement.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
+    """
+    return accumulate(add, 0, n, term)
+
+def product_using_accumulate(n, term):
+    """Returns the product: term(1) * ... * term(n), using accumulate.
+
+    >>> product_using_accumulate(4, square) # square(1) * square(2) * square(3) * square()
+    576
+    >>> product_using_accumulate(6, triple) # triple(1) * triple(2) * ... * triple(5) * triple(6)
+    524880
+    >>> # This test checks that the body of the function is just a return statement.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
+    """
+    return accumulate(mul, 1, n, term)
 
 def main():
     identity = lambda x : x
     square = lambda x : x**2
     triple = lambda x : x**3
-    print(product(5, identity))
-    print(product(3, square))
-    print(product(3, triple))
+    print(summation_using_accumulate(5, triple))
+    print(product_using_accumulate(4, square))
+    print(accumulate(lambda x, y: x + y + 1, 2, 3, square))
 main()
