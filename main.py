7859a78a5ea65e27cc42e8a7a14a21f442b4483c
marbel <othmane.xx90@gmail.com>
2024-11-29 19:34:39 +0100

    number of eight

--- main.py
+++ main.py
@@ -132,11 +132,38 @@ def make_repeater(f, n):
         g = compose(g,f)
     return g
 
+def num_eights(n):
+    """Returns the number of times 8 appears as a digit of n.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> num_eights(8782089)
+    3
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
+    True
+    """
+    x = 0
+    if n % 10 == 8:
+        x = 1
+    if n // 10 == 0:
+        return x
+    else:
+        return  x + num_eights(n // 10)
+
+
 def main():
-    identity = lambda x : x
-    square = lambda x : x**2
-    triple = lambda x : x**3
-    print(make_repeater(triple, 5)(1))
-    print(make_repeater(square, 2)(5))
-    print(make_repeater(square, 3)(5))
+    print(num_eights(388))
 main()
